c1578a1d861756b38193b9849611653d191dbb57
Mitchel.Paulin <Mitchel0022@gmail.com>
2021-02-20 13:06:12 -0500

    Updated readme

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div class="Ribbon">
-        <a href="www.youtube.com" style={{ color: 'white', textDecoration: 'none' }}>
+        <a href="https://github.com/MitchelPaulin/YAHNW" style={{ color: 'white', textDecoration: 'none' }}>
           <p>YAHNW</p>
         </a>
       </div>
